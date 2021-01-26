6857ceaba8917b91ea0d90f1926f704bf153753e
Hadhiy69 <78009323+Hadhiy69@users.noreply.github.com>
2021-01-26 13:57:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const  BotName  =  'HADHIY BOT☄' ;
+const  instagram  =  'https://instagram.com/hadhiy69.id' ;
+const  telegram  =  'https://t.me/Hadhiy69' ;
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+Const - 
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
